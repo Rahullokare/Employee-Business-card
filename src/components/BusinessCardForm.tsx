f5b074d2ac030fee157fd89cadc5d8a8d6de9e27
Rahul Lokare <58898567+Rahullokare@users.noreply.github.com>
2025-06-19 05:40:53 +0530

    deploy issue

--- src/components/BusinessCardForm.tsx
+++ src/components/BusinessCardForm.tsx
@@ -12,7 +12,6 @@ import {
   User,
   Briefcase,
   Smartphone,
-  Globe,
 } from "lucide-react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
@@ -93,6 +92,7 @@ export function BusinessCardForm() {
             contentType: "image/png",
             upsert: true,
           });
+        console.log(uploadData);
 
         if (uploadError) throw uploadError;
 
@@ -129,12 +129,12 @@ export function BusinessCardForm() {
 
   return (
     <div className="max-w-4xl mx-auto p-4 md:p-8">
-      <div className="bg-gradient-to-r from-blue-600 to-indigo-400 rounded-2xl shadow-xl overflow-hidden">
+      <div className="bg-gradient-to-r from-blue-600 to-indigo-500 rounded-2xl shadow-xl overflow-hidden">
         {/* Header */}
         <div className="bg-white/10 backdrop-blur-sm p-6 text-center">
-          <div className="flex justify-center mb-4">
+          {/* <div className="flex justify-center mb-4">
             <img src={CompanyLogo} alt="Infimatrix" className="h-10" />
-          </div>
+          </div> */}
           <h1 className="text-3xl font-bold text-white">
             Employee Digital Business Card Generator
           </h1>
